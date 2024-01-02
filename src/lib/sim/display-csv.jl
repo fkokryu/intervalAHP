05d08f26f997b14d41057112df6b8b4e0fe6a72b
fkokryu <147470382+fkokryu@users.noreply.github.com>
2024-01-02 14:58:34 +0000

    1-2

--- src/lib/sim/display-csv.jl
+++ src/lib/sim/display-csv.jl
@@ -1,31 +1,31 @@
 using Random
 using CSV
 using DataFrames
+using Base.Threads
 
-# ランダムに5つのPCMを選択する関数
+# Mutexの初期化
+mutex = Threads.Mutex()
+
+# ランダムにPCMを選択する関数
 function random_select_pcms(pcms, count)
     return pcms[shuffle(1:length(pcms))[1:count]]
 end
 
 # 結果を保存するためのデータフレームを初期化
 results = DataFrame(
-    #W_PerfectIncorporation = String[],
     d_PerfectIncorporation = Float64[],
-    #W_tPerfectIncorporation2 = String[],
     d_tPerfectIncorporation2 = Float64[],
-    #W_CommonGround = String[],
     d_CommonGround = Float64[],
-    #W_tCommonGround2 = String[],
     d_tCommonGround2 = Float64[],
-    #W_PartialIncorporation = String[],
     d_PartialIncorporation = Float64[],
-    #W_tPartialIncorporation2 = String[],
     d_tPartialIncorporation2 = Float64[]
 )
 
 # 1000回の実験を実行
-for _ in 1:1000
-    selected_pcms = random_select_pcms(pcms, 5)
+Threads.@threads for _ in 1:1000
+    selected_pcms = random_select_pcms(pcms, 3)
+
+    # 各手法の計算とエラーチェック
 
     # PerfectIncorporationの計算
     PerfectIncorporation = solveonePerfectIncorporationLP(selected_pcms)
@@ -57,21 +57,20 @@ for _ in 1:1000
         tPartialIncorporation2 = (W = "", optimalValue = -100)
     end
 
-    # 結果をデータフレームに追加
-    push!(results, (
-        #W_PerfectIncorporation = join(PerfectIncorporation.W, ","),
+    # スレッドごとの結果を一時的に保存
+    local_results = (
         d_PerfectIncorporation = PerfectIncorporation.optimalValue,
-        #W_tPerfectIncorporation2 = join(tPerfectIncorporation2.W, ","),
         d_tPerfectIncorporation2 = tPerfectIncorporation2.optimalValue,
-        #W_CommonGround = join(CommonGround.W, ","),
         d_CommonGround = CommonGround.optimalValue,
-        #W_tCommonGround2 = join(tCommonGround2.W, ","),
         d_tCommonGround2 = tCommonGround2.optimalValue,
-        #W_PartialIncorporation = join(PartialIncorporation.W, ","),
         d_PartialIncorporation = PartialIncorporation.optimalValue,
-        #W_tPartialIncorporation2 = join(tPartialIncorporation2.W, ","),
         d_tPartialIncorporation2 = tPartialIncorporation2.optimalValue
-    ))
+    )
+
+    # Mutexを使用してスレッドセーフに結果を追加
+    lock(mutex) do
+        push!(results, local_results)
+    end
 end
 
 # 結果をCSVファイルに保存
